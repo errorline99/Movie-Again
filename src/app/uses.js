ef5cc5ef03d6b17c75daa97434e1491f17f05f57
errorline99 <100209216+errorline99@users.noreply.github.com>
2024-05-31 02:04:08 +0300

    Update uses.js

--- src/app/uses.js
+++ src/app/uses.js
@@ -7,24 +7,24 @@ import {vv} from './api'
 
 export default function Uses(props) {
 
-  useEffect(() => {
-    var j = document.querySelector('.img-cov')
-    var b = document.querySelectorAll('.image-wrapper img')
-    var k = document.querySelectorAll('.image-wrapper p')
-    var paper = document.querySelector('.img-text')
-    var kk = props.kook
-    var spoiler = props.su
+  // useEffect(() => {
+  //   var j = document.querySelector('.img-cov')
+  //   var b = document.querySelectorAll('.image-wrapper img')
+  //   var k = document.querySelectorAll('.image-wrapper p')
+  //   var paper = document.querySelector('.img-text')
+  //   var kk = props.kook
+  //   var spoiler = props.su
 
-    k.forEach((element , index, array) => {
-      element.innerHTML = kk[index].substring(0, 15) + '<br>' + kk[index].substring(15);
-    })
+  //   k.forEach((element , index, array) => {
+  //     element.innerHTML = kk[index].substring(0, 15) + '<br>' + kk[index].substring(15);
+  //   })
 
-    b.forEach((element, index, array) => {
-      element.addEventListener('click', function() {
-        j.src = element.src
-        paper.innerHTML = spoiler[index]
-      })
-    })
+  //   b.forEach((element, index, array) => {
+  //     element.addEventListener('click', function() {
+  //       j.src = element.src
+  //       paper.innerHTML = spoiler[index]
+  //     })
+  //   })
 
 
 
