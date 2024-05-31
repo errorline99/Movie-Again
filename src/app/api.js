858c28efe0dd84d20fac8f242e2a11e92abee33b
errorline99 <100209216+errorline99@users.noreply.github.com>
2024-06-01 01:16:03 +0300

    Update api.js

--- src/app/api.js
+++ src/app/api.js
@@ -6,9 +6,7 @@ import Client from './wish'
 
 export default async function Gone(props) {
 
-  const items = props.image
-  const title = props.titles
-  const overviews = props.summa
+
 
 
   return (
