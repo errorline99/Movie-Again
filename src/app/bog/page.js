de43c03bcd87263793e2121c66c8d88d881cde66
errorline99 <100209216+errorline99@users.noreply.github.com>
2024-06-01 01:20:12 +0300

    Update page.js

--- src/app/bog/page.js
+++ src/app/bog/page.js
@@ -79,66 +79,14 @@ const weekRated = async () => {
 
 
 export default async function top(props){
-    var getIt = await rated()
-    var getweek = await weekRated()
-    var weekImgs = getweek[0]
-    var weekblight = getweek[1]
-    var weekTi = getweek[2]
-    var lineBreak = getIt[2]
-    var lineBreak2 = getIt[3]
-    var lineBreak3 = getIt[4]
-    var lineBreak4 = getIt[5]
-    var lineBreak5 = getIt[6]
-    var lineBreak6 = getIt[7]
-    var score = getIt[9]
 
 
 
 
 
     return(
-        <div className="kkl">
-            <div className="box-man">
-            <img className="back-img" src={getIt[1]} alt={""}/>
-            <p className="wallTitle">{lineBreak6} </p>
-            </div>
-            <br />
-            <div className="hii-con">
-              <div>
-                <h3 className="hii-cli">All time Movies</h3></div>
-            </div>
-  
-            <div className="image-flask">
-              {getIt[0].map((user, index) => <div key={index} className="image-wrapper">
-            <img className='try' id={'' +index} src={user} alt={""}/>
-            <p className="image-name"></p>
-            <div className="star-line">
-              <span className="stars"></span>
-            </div>
-
-            
-            <div>
-            </div>
-          </div>
-          )}
-            </div>
-
-            <div className="week-block">
-              <h3 className="week">Weekly Trending</h3>
-            </div>
-
-            <div className="imgae-flask-week">
-            {weekImgs.map((user, index) => <div key={index} className="image-wrapper">
-            <img className='try' id={'' +index} src={user} alt={""}/>
-            <p className="image-title"></p>
-            <div className="star-line">
-              <span className="westars"></span>
-            </div>
-          </div>
-          )}
-            </div>
-
-          <Cliz point={score}  name={getIt[8]} weekFlash={weekblight} weekName={weekTi}/>
-        </div>
+      <div>
+      hooooooooooooo
+      </div>
     )
 }
